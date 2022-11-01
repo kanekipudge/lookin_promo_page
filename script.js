7a682ad624ce7f831bcb6568c0460be5584fe74e
Vladimir <87974637+kanekipudge@users.noreply.github.com>
2022-11-01 14:18:15 +0300

    Add files via upload
    Upgraded big logo + scale change due to click

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-<script>
+$(document).ready(function() {
+  $('.block_item').click(function(event) {
+    const clicked = $(this);
+    if ($('.block').hasClass('one')) {
+      $('.block_item.active').not(clicked).removeClass('active');
+      $('.block_content').not(clicked.children('.block_content')).slideUp(300);
+    }
+    if (clicked.hasClass('active')) {
+      $('.lookin_big_logo').toggleClass('active');
+      $('.footerlast').toggleClass('active');
+    } else {
+      $('.lookin_big_logo').addClass('active');
+      $('.footerlast').addClass('active');
+    }
+    $('.lookin_big_logo').remove('active');
+    clicked.toggleClass('active');
+    clicked.children('.block_title').toggleClass('p').next().slideToggle(300);
 
-</script>
+  });
+});
